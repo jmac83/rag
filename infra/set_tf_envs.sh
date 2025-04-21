063c7fa32111f669675565edbda83d67b1fd9146
Maciej B <554091+jmac83@users.noreply.github.com>
2025-04-21 16:12:54 +0200

    added envs for function app url and key

--- infra/set_tf_envs.sh
+++ infra/set_tf_envs.sh
@@ -9,5 +9,7 @@ export AZURE_OPENAI_ENDPOINT=$(terraform output -raw open_ai_endpoint_url)
 export AzureWebJobsStorage=$(terraform output -raw function_storage_account_connection_string)
 export UPLOAD_STORAGE_CONNECTION_STRING=$(terraform output -raw upload_storage_account_connection_string)
 export UPLOAD_BLOB_PATH="uploads/{name}"
+export AZURE_FUNCTION_APP_URL=$(terraform output -raw function_app_url)
+export AZURE_FUNCTION_APP_KEY=$(terraform output -raw function_app_key)
 
 echo "Done."
